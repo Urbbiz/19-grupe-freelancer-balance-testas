da29bfde9e22634b83cb77e8df3bf053cedd7e59
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-29 14:07:13 +0200

    menesiai.js added

---
+++ js/menesiai.js
@@ -0,0 +1,14 @@
+const menesiai = [
+    'Sausis',
+    'Vasaris',
+    'Kovas',
+    'Balandis',
+    'Gegužė',
+    'Birželis',
+    'Liepa',
+    'Rugpjūtis',
+    'Rugsėjis',
+    'Spalis',
+    'Lapkritis',
+    'Gruodis'
+];
